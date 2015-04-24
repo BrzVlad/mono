73586a3baa9b9ba8330cbe30dd8b2b439af86c31
Vlad Brezae <brezaevlad@gmail.com>
2015-04-24 12:02:47 -0700

    [jit] Include the unwind_info into the tramp JitInfos

--- mono/mini/mini-runtime.c
+++ mono/mini/mini-runtime.c
@@ -479,7 +479,10 @@ register_trampoline_jit_info (MonoDomain *domain, MonoTrampInfo *info)
 	mono_jit_info_init (ji, NULL, info->code, info->code_size, 0, 0, 0);
 	ji->d.tramp_info = info;
 	ji->is_trampoline = TRUE;
-	// FIXME: Unwind info
+	ji->code_start = info->code;
+	ji->code_size = info->code_size;
+
+	ji->unwind_info = mono_cache_unwind_info (info->uw_info, info->uw_info_len);
 
 	mono_jit_info_table_add (domain, ji);
 }
@@ -503,6 +506,13 @@ mono_tramp_info_register (MonoTrampInfo *info)
 	copy->code = info->code;
 	copy->code_size = info->code_size;
 	copy->name = g_strdup (info->name);
+
+	if (info->unwind_ops) {
+		copy->uw_info = mono_unwind_ops_encode (info->unwind_ops, &copy->uw_info_len);
+	} else {
+		copy->uw_info = info->uw_info;
+		copy->uw_info_len = info->uw_info_len;
+	}
 
 	mono_jit_lock ();
 	tramp_infos = g_slist_prepend (tramp_infos, copy);
